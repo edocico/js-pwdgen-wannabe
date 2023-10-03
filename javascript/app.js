3e0a8a29224cb19d2f27db93b53076faf9aff17b
edocico <138378767+edocico@users.noreply.github.com>
2023-10-03 17:44:15 +0200

    small correction of ; in js

--- javascript/app.js
+++ javascript/app.js
@@ -4,38 +4,48 @@
 
 //## Chiedere all' utente il nome
     //- dichiarare una variabile per il nome utente
-    let firstName;
-    //- assegnare alla variabile il valore restituito da un prompt sulla pagina
-    firstName = prompt('Ciao! Dimmi il tuo nome...');
-    console.log(firstName)
+let firstName;
+
+//- assegnare alla variabile il valore restituito da un prompt sulla pagina
+firstName = prompt('Ciao! Dimmi il tuo nome...');
+console.log(firstName);
+
 //## Chiedere all' utente il cognome
-    //- dichiarare una variabile per il cognome dell' utente
-    let lastName;
-    //- assegnare alla variabile il valore restituito da un prompt sulla pagina
-    lastName = prompt('E adesso il tuo cognome...');
-    console.log(lastName)
+//- dichiarare una variabile per il cognome dell' utente
+let lastName;
+
+//- assegnare alla variabile il valore restituito da un prompt sulla pagina
+lastName = prompt('E adesso il tuo cognome...');
+console.log(lastName);
+
 //## Chiedere all' utente il colore preferito
-    //- dichiarare una variabile per il colore preferito dell' utente
-    let preferredColour;
-    //- assegnare alla variabile il valore restituito da un prompt sulla pagina
-    preferredColour = prompt('Dimmi il tuo colore preferito...');
-    console.log(preferredColour)
+//- dichiarare una variabile per il colore preferito dell' utente
+let preferredColour;
+
+//- assegnare alla variabile il valore restituito da un prompt sulla pagina
+preferredColour = prompt('Dimmi il tuo colore preferito...');
+console.log(preferredColour);
+
 //## Genera randomicamente un numero 
-    //### Bonus
-       // - Generare randomicamente un numero
-       let genNumber;
-       genNumber = Math.floor((Math.random() * 100) + 1);
-       console.log(genNumber) 
-       // -trasformare il valore numerico in una stringa
-       let stringNumber;
-        stringNumber = genNumber.toString();
-        console.log(stringNumber)
+//### Bonus
+// - Generare randomicamente un numero
+let genNumber;
+genNumber = Math.floor((Math.random() * 100) + 1);
+console.log(genNumber);
+
+// -trasformare il valore numerico in una stringa
+let stringNumber;
+stringNumber = genNumber.toString();
+console.log(stringNumber);
+
 //## Concatenare nome , cognome, colore e numero
-    //- dichiarare una variabile "new password " dove concatenare le varie stringhe
-    const newPassword = firstName + lastName + preferredColour + stringNumber;
-    console.log(newPassword)
+//- dichiarare una variabile "new password " dove concatenare le varie stringhe
+const newPassword = firstName + lastName + preferredColour + stringNumber;
+console.log(newPassword);
+
 //## Stampare il risultato sulla pagina
-    //- richiamare con javascript l'elemento del dom con id = password
-        const printedPassword = document.getElementById('password');
-    //- modificare l' innerHTML dell' elemento con id = password con il valore della variabile " new password "
-        printedPassword.innerHTML = newPassword;
+//- richiamare con javascript l'elemento del dom con id = password
+const printedPassword = document.getElementById('password');
+
+//- modificare l' innerHTML dell' elemento con id = password con il valore della variabile " new password "
+printedPassword.innerHTML = newPassword;
