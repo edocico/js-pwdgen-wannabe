6de0d6b4ee3a521a019e9531257271ccd4d528e0
edocico <138378767+edocico@users.noreply.github.com>
2023-10-03 16:04:49 +0200

    var and prompt for name and lastname

--- javascript/app.js
+++ javascript/app.js
@@ -1,2 +1,28 @@
 //document.writeln('hello world')
 
+//# Password Generator
+
+//## Chiedere all' utente il nome
+    //- dichiarare una variabile per il nome utente
+    let firstName
+    //- assegnare alla variabile il valore restituito da un prompt sulla pagina
+    firstName = prompt('Ciao! Dimmi il tuo nome...')
+    console.log(firstName)
+//## Chiedere all' utente il cognome
+    //- dichiarare una variabile per il cognome dell' utente
+    let lastName
+    //- assegnare alla variabile il valore restituito da un prompt sulla pagina
+    lastName = prompt('E adesso il tuo cognome...')
+    console.log(lastName)
+//## Chiedere all' utente il colore preferito
+    //- dichiarare una variabile per il colore preferito dell' utente
+    //- assegnare alla variabile il valore restituito da un prompt sulla pagina
+//## Genera randomicamente un numero 
+    //### Bonus
+       // - Generare randomicamente un numero 
+       // -trasformare il valore numerico in una stringa
+//## Concatenare nome , cognome, colore e numero
+    //- dichiarare una variabile "new password " dove concatenare le varie stringhe 
+//## Stampare il risultato sulla pagina
+    //- richiamare con javascript l'elemento del dom con id = password
+    //- modificare l' innerHTML dell' elemento con id = password con il valore della variabile " new password "
